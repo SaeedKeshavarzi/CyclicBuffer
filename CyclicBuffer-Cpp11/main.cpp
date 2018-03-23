f3495e9cf87c8eb4facf44e6cfe35e457ae5b4f7
Saeed Keshavarzi <keshavarzi1991@gmail.com>
2018-03-23 09:53:35 -0700

    Update main.cpp

--- CyclicBuffer-Cpp11/main.cpp
+++ CyclicBuffer-Cpp11/main.cpp
@@ -1,44 +1,71 @@
+//#define EXTERNAL_PACKET
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <thread>
 
 #include "cyclic_buffer.h"
 #include "thread_naming.h"
 
-cyclic_buffer<false> buffer(10, 8, 3, 3);
+cyclic_buffer<true> buffer(10, 8, 3, 3);
 volatile bool is_finished{ false };
 
 void producer()
 {
-	int32_t *data = new int32_t[1]{ 0 };
-	int32_t cnt = 0;
+#ifdef EXTERNAL_PACKET
+	int32_t *data = new int32_t[1];
 
-	while (!is_finished)
+	for (int32_t cnt = 0; !is_finished; ++cnt)
 	{
+		*data = cnt;
 		buffer.push((char**)(&data));
-		(*data) = ++cnt;
 	}
 
-	printf("producer say goodbye\n");
 	delete[] data;
+#else
+	for (int32_t cnt = 0; !is_finished; ++cnt)
+	{
+		**(int32_t**)buffer.get_write_packet() = cnt;
+		buffer.push();
+	}
+#endif // EXTERNAL_PACKET
+
+	printf("producer say goodbye\n");
 }
 
 void consumer()
 {
-	int32_t *data = new int32_t[1], cnt;
-	int32_t curr, last{ -1 };
+#ifdef EXTERNAL_PACKET
+	int32_t *data{ new int32_t[1] };
+#else
+	int32_t *data{ *(int32_t**)buffer.get_read_packet() };
+#endif // EXTERNAL_PACKET
 
-	auto _1 = std::chrono::high_resolution_clock::now();
+	int32_t cnt, curr, last{ -1 };
+
+	auto _1{ std::chrono::high_resolution_clock::now() };
 
 	for (cnt = 0; cnt < 100000000; ++cnt)
 	{
 		*data = -(1 + cnt);
+
+#ifdef EXTERNAL_PACKET
 		buffer.pop((char**)(&data));
+#else
+		buffer.pop();
+		data = *(int32_t**)buffer.get_read_packet();
+#endif // EXTERNAL_PACKET
 
 		curr = *data;
 
-		// if ((curr < 0) || (curr < last))
-		if (curr != cnt)
+		if (buffer.is_lock_free)
+		{
+			if ((curr < 0) || (curr < last))
+			{
+				printf("Error: %d : %d \n", cnt, *data);
+			}
+		}
+		else if (curr != cnt)
 		{
 			printf("Error: %d : %d \n", cnt, *data);
 		}
@@ -46,7 +73,7 @@ void consumer()
 		last = curr;
 	}
 
-	auto _2 = std::chrono::high_resolution_clock::now();
+	auto _2{ std::chrono::high_resolution_clock::now() };
 
 	is_finished = true;
 	printf("%d : %d : %f\n", cnt - 1, *data, (double)(*data) / (cnt - 1));
@@ -55,7 +82,10 @@ void consumer()
 	printf("%I64d ms\n", diff);
 
 	buffer.terminate();
+
+#ifdef EXTERNAL_PACKET
 	delete[] data;
+#endif // EXTERNAL_PACKET
 }
 
 int main()
