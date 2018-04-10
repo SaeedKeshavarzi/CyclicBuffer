26f4ff059ee870975d53d158886d4666ff9657b6
Saeed Keshavarzi <keshavarzi1991@gmail.com>
2018-04-10 08:43:58 -0700

    update main.cpp with new cyclic_buffer

--- CyclicBuffer-Cpp11/main.cpp
+++ CyclicBuffer-Cpp11/main.cpp
@@ -1,5 +1,7 @@
-//#define EXTERNAL_PACKET
 #define _CRT_SECURE_NO_WARNINGS
+
+#define IS_LOCK_FREE true
+#define IS_RECYCLABLE false
 
 #include <stdio.h>
 #include <thread>
@@ -7,67 +9,36 @@
 #include "cyclic_buffer.h"
 #include "thread_naming.h"
 
-cyclic_buffer<int32_t, true> buffer(10, 1, 3, 3);
-volatile bool is_finished{ false };
+cyclic_buffer<int32_t, IS_LOCK_FREE, IS_RECYCLABLE> buffer(10);
 
 void producer()
 {
-#ifdef EXTERNAL_PACKET
-	int32_t *data = new int32_t();
-
-	for (int32_t cnt = 0; !is_finished; ++cnt)
+	for (int32_t cnt = 0; !buffer.is_terminated(); ++cnt)
 	{
-		*data = cnt;
-		buffer.push(&data);
+		buffer.push(cnt);
 	}
-
-	delete data;
-#else
-	for (int32_t cnt = 0; !is_finished; ++cnt)
-	{
-		*buffer.get_write_packet() = cnt;
-		buffer.push();
-	}
-#endif // EXTERNAL_PACKET
 
 	printf("producer say goodbye\n");
 }
 
 void consumer()
 {
-#ifdef EXTERNAL_PACKET
-	int32_t *data{ new int32_t() };
-#else
-	int32_t *data{ buffer.get_read_packet() };
-#endif // EXTERNAL_PACKET
-
 	int32_t cnt, curr, last{ -1 };
 
+	buffer.wait_for_data();
 	auto _1{ std::chrono::high_resolution_clock::now() };
 
 	for (cnt = 0; cnt < 100000000; ++cnt)
 	{
-		*data = -(1 + cnt);
-
-#ifdef EXTERNAL_PACKET
-		buffer.pop(&data);
+#if IS_RECYCLABLE
+		curr = buffer.pop(0);
 #else
-		buffer.pop();
-		data = buffer.get_read_packet();
-#endif // EXTERNAL_PACKET
+		curr = buffer.pop();
+#endif
 
-		curr = *data;
-
-		if (buffer.is_lock_free)
+		if (curr < last)
 		{
-			if ((curr < 0) || (curr < last))
-			{
-				printf("Error: %d : %d \n", cnt, *data);
-			}
-		}
-		else if (curr != cnt)
-		{
-			printf("Error: %d : %d \n", cnt, *data);
+			printf("Error: %d : %d \n", cnt, curr);
 		}
 
 		last = curr;
@@ -75,17 +46,11 @@ void consumer()
 
 	auto _2{ std::chrono::high_resolution_clock::now() };
 
-	is_finished = true;
-	printf("%d : %d : %f\n", cnt - 1, *data, (double)(*data) / (cnt - 1));
+	buffer.terminate();
+	printf("%d : %d : %f\n", cnt - 1, curr, (double)curr / (cnt - 1));
 
 	auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(_2 - _1).count();
 	printf("%I64d ms\n", diff);
-
-	buffer.terminate();
-
-#ifdef EXTERNAL_PACKET
-	delete data;
-#endif // EXTERNAL_PACKET
 }
 
 int main()
