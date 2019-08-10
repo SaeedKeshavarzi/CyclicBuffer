d48ee854b57f2cdb927bf30e51628851ad41f78b
Saeed Keshavarzi <keshavarzi1991@gmail.com>
2019-08-11 02:03:45 +0430

    Update sync_thread.h

--- CyclicBuffer-Cpp11/sync_thread.h
+++ CyclicBuffer-Cpp11/sync_thread.h
@@ -3,38 +3,45 @@
 
 #include <atomic>
 #include <mutex>
-#include <shared_mutex>
 #include <condition_variable>
 
 class sync_thread_t
 {
-private:
-	mutable std::mutex sync_guard;
-	mutable std::shared_mutex config_guard;
-	mutable std::condition_variable cv;
-	std::atomic_size_t n_threads{ 0 };
-	std::atomic_size_t n_involved_thread{ 0 };
-	std::atomic_bool terminated{ false };
-
 public:
 	sync_thread_t() = default;
 	sync_thread_t(const sync_thread_t&) = delete;
 	sync_thread_t& operator=(const sync_thread_t&) = delete;
 
-	sync_thread_t(std::size_t _n_threads) :
-		n_threads{ _n_threads }
+	sync_thread_t(std::size_t _n_threads)
 	{
-
+		n_threads = _n_threads;
 	}
 
 	inline ~sync_thread_t()
 	{
-		terminate();
+		enable(false);
 	}
 
-	inline bool is_terminated() const
+	inline bool enable() const
 	{
-		return terminated;
+		return on;
+	}
+
+	inline void enable(bool value)
+	{
+		if (value)
+			on = true;
+		else
+		{
+			std::lock_guard<std::mutex> lock(guard);
+
+			on = false;
+			if (n_involved_thread > 0)
+			{
+				n_involved_thread = 0;
+				cv.notify_all();
+			}
+		}
 	}
 
 	inline std::size_t thread_count() const
@@ -44,72 +51,60 @@ public:
 
 	inline void thread_count(std::size_t count)
 	{
-		std::lock_guard<std::shared_mutex> lock(config_guard);
+		std::lock_guard<std::mutex> lock(guard);
 
 		n_threads = count;
+		if ((n_involved_thread > 0) && (n_involved_thread >= n_threads))
+		{
+			n_involved_thread = 0;
+			cv.notify_all();
+		}
 	}
 
 	inline void register_thread()
 	{
-		std::lock_guard<std::shared_mutex> lock(config_guard);
+		std::lock_guard<std::mutex> lock(guard);
 
 		n_threads.fetch_add(1);
 	}
 
 	inline void unregister_thread()
 	{
-		std::lock_guard<std::shared_mutex> lock(config_guard);
+		std::lock_guard<std::mutex> lock(guard);
 
 		n_threads.fetch_sub(1);
-	}
-
-	inline void reset()
-	{
-		std::lock_guard<std::shared_mutex> lock(config_guard);
-
-		n_threads = 0;
-		n_involved_thread = 0;
-		terminated = false;
-	}
-
-	inline void terminate()
-	{
-		std::lock_guard<std::mutex> lock(sync_guard);
-
-		n_involved_thread = 0;
-		terminated = true;
-		cv.notify_all();
+		if ((n_involved_thread > 0) && (n_involved_thread >= n_threads))
+		{
+			n_involved_thread = 0;
+			cv.notify_all();
+		}
 	}
 
 	inline bool sync()
 	{
-		std::shared_lock<std::shared_mutex> config_lock(config_guard);
-		std::unique_lock<std::mutex> lock(sync_guard);
+		std::unique_lock<std::mutex> lock(guard);
 
-		if (terminated || (n_involved_thread >= n_threads))
+		if (!on || (n_threads == 0))
 			return false;
 
 		++n_involved_thread;
 		if (n_involved_thread < n_threads)
-		{
 			cv.wait(lock);
-		}
 		else // (n_involved_thread == n_threads)
 		{
 			n_involved_thread = 0;
 			cv.notify_all();
 		}
 
-		return (!terminated);
+		return on;
 	}
 
 	template<class _Rep, class _Period>
 	inline bool sync_for(const std::chrono::duration<_Rep, _Period>& rel_time)
 	{
-		std::shared_lock<std::shared_mutex> config_lock(config_guard);
-		std::unique_lock<std::mutex> lock(sync_guard);
+		std::unique_lock<std::mutex> lock(guard);
 
-		if (terminated || (n_involved_thread >= n_threads))
+		if (!on || (n_threads == 0))
 			return false;
 
 		++n_involved_thread;
@@ -127,16 +122,15 @@ public:
 			cv.notify_all();
 		}
 
-		return (!terminated);
+		return on;
 	}
 
 	template<class _Clock, class _Duration>
 	inline bool sync_until(const std::chrono::time_point<_Clock, _Duration>& timeout_time)
 	{
-		std::shared_lock<std::shared_mutex> config_lock(config_guard);
-		std::unique_lock<std::mutex> lock(sync_guard);
+		std::unique_lock<std::mutex> lock(guard);
 
-		if (terminated || (n_involved_thread >= n_threads))
+		if (!on || (n_threads == 0))
 			return false;
 
 		++n_involved_thread;
@@ -154,8 +148,15 @@ public:
 			cv.notify_all();
 		}
 
-		return (!terminated);
+		return on;
 	}
+
+private:
+	mutable std::mutex guard;
+	mutable std::condition_variable cv;
+	std::atomic_size_t n_threads{ 0 };
+	std::atomic_size_t n_involved_thread{ 0 };
+	std::atomic_bool on{ true };
 };
 
 #endif // !_SYNC_THREAD_H_
